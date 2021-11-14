80eb1c9ea1770ed196047382d714ba839c664c19
dominicom <dominik.kiepuszewski@gmail.com>
2021-11-14 19:56:21 +0100

    Reverted fix for calc max zoom in Baselayer component  - not worked properly

--- src/components/Map/Layers/BaseLayer.js
+++ src/components/Map/Layers/BaseLayer.js
@@ -8,10 +8,17 @@ import MapTileError from '../../../images/map_tile_error.png';
 // https://github.com/Zverik/Leaflet.LimitZoom
 
 const BaseLayer = props => {
-  const { activeMap, map, zoom, maxZoom } = props;
+  const { 
+    activeMap, 
+    map 
+    // zoom, 
+    // maxZoom 
+  } = props;
 
-  const calcZoom = zoom > maxZoom;
+  // const calcZoom = zoom > maxZoom;
   // console.log(activeMap);
+
+
   if (activeMap && activeMap.maxZoom) {
     return (
       <TileLayer 
